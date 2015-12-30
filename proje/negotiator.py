487b630d8bd94564854db9c5389193b9ac3d182a
defneeskiocak <defneeskiocak@gmail.com>
2015-12-30 21:45:47 +0200

    Update negotiator.py

--- proje/negotiator.py
+++ proje/negotiator.py
@@ -2,6 +2,7 @@ import threading
 import Queue
 import socket
 import time
+import sys
 
 connect_point_list = {}
 
@@ -48,7 +49,22 @@ class serverReadThread (threading.Thread):
 
     def run(self):
         print "Starting negotiator's server read thread"
-        data = self.socket.recv()
+        data = self.socket.recv(1024)
         self.parser(data)
 
 
+host = "127.0.0.1"
+port = 12345
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "Soket olusturuldu"
+
+try:
+    s.bind((host, port))
+except socket.error as msg:
+    print "Bind basarisiz. Hata kodu: " + str(msg[0]) + " Message " + msg[1]
+    sys.exit()
+
+print "Soket bind basarili"
+
+s.listen(10)
+print "Soket dinlemede"
